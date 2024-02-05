31ec5eef4406c1f3bb81048a96d352391d622c50
HeoYoungGyo <gjdudry@naver.com>
2024-02-05 20:56:49 +0900

    초기 커밋.

---
+++ Source/UntilDawn/Public/UI/Login/WidgetLoginScreen.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "WidgetLoginScreen.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNTILDAWN_API UWidgetLoginScreen : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/UntilDawn/UntilDawnGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UntilDawnGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNTILDAWN_API AUntilDawnGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
